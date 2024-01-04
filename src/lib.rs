3d215cb39d89cd3a568f060afa7fa9e3b4237694
chping <chping@iowk.com>
2024-01-04 21:59:59 +0800

    post

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
 use std::collections::HashMap;
 
 use reqwest::Request;
+use serde::Serialize;
+use thiserror::Error;
 use tokio::signal;
 use urand::generate_unique_number;
-
+use url::ParseError;
+use url::Url;
 //模块
-mod urand;
 mod timestamp;
 mod uhmac;
+mod urand;
 use url::form_urlencoded;
 
 fn get_query(query: HashMap<&str, &str>) -> String {
@@ -20,99 +23,166 @@ fn get_query(query: HashMap<&str, &str>) -> String {
     encoded_query
 }
 
-
 #[derive(Debug, Clone)]
-pub struct VxwkConfig{
-    pub access_key:String,
-    pub access_secret:String,
-    pub endpoint:String
+pub struct VxwkConfig {
+    pub access_key: String,
+    pub access_secret: String,
+    pub endpoint: String,
 }
 
-pub enum  VxwkError {
-    ReqwestError(reqwest::Error),
+#[derive(Debug, Error)]
+pub enum VxwkError {
+    #[error("http request error")]
+    ReqwestError(#[from] reqwest::Error),
+    #[error("url parse error")]
+    ParseError(#[from] ParseError),
+    #[error("Unkonow error `{0}` ")]
     OtherError(String),
+    #[error("Invalid response `{0}`")]
     InvalidResponse(String),
+    #[error("Invalid Request `{0}`")]
     InvalidRequest(String),
-    InvalidSignature(String),
-    InvalidTimestamp(String),
+    #[error("the data for key `{0}`")]
     InvalidAccessKey(String),
+    #[error("the data for key `{0}`")]
     InvalidAccessSecret(String),
+    #[error("Invalid Endpoint `{0}`")]
     InvalidEndpoint(String),
-    InvalidQuery(String),
-    InvalidBody(String),
-    InvalidHeader(String),
-    InvalidContentType(String),
-    InvalidContentLength(String),
-    InvalidContentMD5(String),
 }
 
-impl VxwkConfig{
-    pub fn new( access_key:String,access_secret:String, endpoint:String)->Self{
-        Self { access_key, access_secret, endpoint }
+impl VxwkConfig {
+    pub fn new(access_key: String, access_secret: String, endpoint: String) -> Self {
+        Self {
+            access_key,
+            access_secret,
+            endpoint,
+        }
     }
 }
 
-
-
-pub struct  VxwkAPI {
+pub struct VxwkAPI {
     client: reqwest::Client,
     config: VxwkConfig,
-    accept_header: Option<HashMap<String,String>>,
+    accept_header: Option<HashMap<String, String>>,
 }
 
-// fn get_auth_header(config:&VxwkConfig)->HashMap<String,String>{
-//     let mut header = HashMap::new();
-//     header.insert(String::from("Authorization"),format!("Bearer {}
-// }
-
-
-
 impl VxwkAPI {
-    pub fn new(config:VxwkConfig)->Self{
+    pub fn new(config: VxwkConfig) -> Self {
         let client = reqwest::Client::new();
-        let accept_header = Some(HashMap::from([(String::from("Accept"),String::from("application/json"))]));
-        Self { client, config, accept_header }
+        let accept_header = Some(HashMap::from([(
+            String::from("Accept"),
+            String::from("application/json"),
+        )]));
+        Self {
+            client,
+            config,
+            accept_header,
+        }
     }
-    fn gen_signature(&self, timestamp:&str, n:&str, path:&str, query:HashMap<&str,&str>)->String{
-         format!("{}\n{}\n{}\n{}\n{}\n{}\n", reqwest::Method::GET, &self.config.access_key, timestamp, n, path, get_query(query))
+    fn gen_signature(
+        &self,
+        timestamp: &str,
+        n: &str,
+        path: &str,
+        query: HashMap<&str, &str>,
+    ) -> String {
+        format!(
+            "{}\n{}\n{}\n{}\n{}\n{}\n",
+            reqwest::Method::GET,
+            &self.config.access_key,
+            timestamp,
+            n,
+            path,
+            get_query(query)
+        )
     }
 
-    /// 用于发送get请求到
-    async fn get(&self, path:&str,query_params:HashMap<&str,&str>) -> Result<serde_json::Value, reqwest::Error>{
-        let mut url = self.config.endpoint.clone();
-        url.push_str(path);
+    /// 用于发送GET请求
+    async fn get(
+        &self,
+        path: &str,
+        query_params: HashMap<&str, &str>,
+    ) -> Result<serde_json::Value, VxwkError> {
+        // 构建完整的URL
+        let mut url = Url::parse(&self.config.endpoint)?;
+        url.set_path(path);
+        url.query_pairs_mut().extend_pairs(query_params.iter());
 
-        let timestamp = format!("{}",timestamp::get_timestamp());
-
+        // 构建签名所需的参数
+        let timestamp = format!("{}", timestamp::get_timestamp());
         let unique_number = generate_unique_number(18);
-
-        let mut  query_build = query_params;
-        query_build.insert("xaccesskey",self.config.access_key.as_str());
-        query_build.insert("xn",&unique_number);
-        query_build.insert("xtimestamp",timestamp.as_str());
-        query_build.insert("xrunmode","release");
         
-        let signature = self.gen_signature(timestamp.as_str(),unique_number.as_str(),path,query_build.clone());
-       
-        query_build.insert("xsignature",signature.as_str());
+        let mut signature_params = query_params.clone();
+        signature_params.insert("xaccesskey", self.config.access_key.as_str());
+        signature_params.insert("xn", &unique_number);
+        signature_params.insert("xtimestamp", &timestamp);
+        signature_params.insert("xrunmode", "release");
+
+        let signature =
+            self.gen_signature(&timestamp, &unique_number, path, signature_params.clone());
+        signature_params.insert("xsignature", &signature);
 
         let sign = uhmac::calculate_hmac(&self.config.access_secret, &signature);
+        signature_params.insert("xsign", &sign);
 
-        query_build.insert("xsign", sign.as_str());
-    
-        //发起get请求
-        let response = self.client.get(url)
-        .query(&query_build)
-        .send().await?;
+        // 添加签名到URL
+        url.query_pairs_mut().extend_pairs(signature_params.iter());
 
-        //获取响应体
+        // 发起GET请求
+        let response = self.client.get(url).send().await?;
+
+        // 获取响应体
         match response.error_for_status_ref() {
             Ok(_) => Ok(response.json().await?),
             Err(err) => {
                 log::error!("Error: {}", err);
-                Err(err)
+                Err(VxwkError::from(err))
             }
         }
     }
+    /// 用于发送POST请求
+    async fn post<T>(&self, path: &str, body: &T) -> Result<serde_json::Value, VxwkError>
+    where
+        T: Serialize,
+    {
+        let mut url = self.config.endpoint.clone();
+        url.push_str(path);
+
+        let timestamp = format!("{}", timestamp::get_timestamp());
+        let unique_number = generate_unique_number(18);
+
+        let mut query_params = HashMap::new();
+        query_params.insert("xaccesskey", self.config.access_key.as_str());
+        query_params.insert("xn", &unique_number);
+        query_params.insert("xtimestamp", timestamp.as_str());
+        query_params.insert("xrunmode", "release");
+
+        let signature = self.gen_signature(
+            timestamp.as_str(),
+            unique_number.as_str(),
+            path,
+            query_params.clone(),
+        );
+        query_params.insert("xsignature", signature.as_str());
+
+        let sign = uhmac::calculate_hmac(&self.config.access_secret, &signature);
+        query_params.insert("xsign", sign.as_str());
+
+        // 构建URL
+        let url_with_params = Url::parse_with_params(&url, query_params)?;
+
+        // 发起POST请求
+        let response = self.client.post(url_with_params).json(body).send().await?;
+
+        // 获取响应体
+        match response.error_for_status_ref() {
+            Ok(_) => Ok(response.json().await?),
+            Err(err) => {
+                log::error!("Error: {}", err);
+                Err(VxwkError::from(err))
+            }
+        }
+    }
+
 
 }
